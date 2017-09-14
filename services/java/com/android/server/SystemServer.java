e4e87a6e37c6c8e69b58769dfc673dfb084ae517
rubis-lab <id@rubis.snu.ac.kr>
2017-09-14 19:29:40 +0900

    add license comment

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * If you want to use as commercial, contact us(rubis.chief@gmail.com).
  */
 
 package com.android.server;
